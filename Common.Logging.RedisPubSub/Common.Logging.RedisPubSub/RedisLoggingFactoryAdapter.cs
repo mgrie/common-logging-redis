c00fc3891a969115adcaa2f552eb275d0f2252f4
Marco Grießhammer <griesshammer@yamud.de>
2019-01-03 22:16:38 +0100

    RedisLoggingFactoryAdapter

---
+++ Common.Logging.RedisPubSub/Common.Logging.RedisPubSub/RedisLoggingFactoryAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging.Configuration;
+
+namespace Common.Logging.Redis
+{
+    public class RedisLoggingFactoryAdapter : Common.Logging.Simple.AbstractSimpleLoggerFactoryAdapter
+    {
+        public RedisLoggingFactoryAdapter(NameValueCollection properties) : base(properties)
+        {
+        }
+
+        public RedisLoggingFactoryAdapter(LogLevel level, bool showDateTime, bool showLogName, bool showLevel, string dateTimeFormat) : base(level, showDateTime, showLogName, showLevel, dateTimeFormat)
+        {
+        }
+
+        protected override ILog CreateLogger(string name, LogLevel level, bool showLevel, bool showDateTime, bool showLogName,
+            string dateTimeFormat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
