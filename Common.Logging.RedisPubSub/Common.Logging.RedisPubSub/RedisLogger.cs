79676f5adda4038ab0d414be1b673329398e00c9
Marco Grießhammer <griesshammer@yamud.de>
2019-01-03 22:17:42 +0100

    RedisLogger

---
+++ Common.Logging.RedisPubSub/Common.Logging.RedisPubSub/RedisLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Logging.RedisPubSub
+{
+    class RedisLogger : Common.Logging.Simple.AbstractSimpleLogger
+    {
+        public RedisLogger(string logName, LogLevel logLevel, bool showlevel, bool showDateTime, bool showLogName, string dateTimeFormat) : base(logName, logLevel, showlevel, showDateTime, showLogName, dateTimeFormat)
+        {
+        }
+
+        protected override void WriteInternal(LogLevel level, object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
